0edb95a29d37a8c07a15e3f7ae8bd7e11234c38a
EmilioMena <emilio.mena128@gmail.com>
2021-05-26 15:55:56 -0400

    added LC 21

---
+++ merge_two_sorted_lists.py
@@ -0,0 +1,28 @@
+"""
+LC 21
+runtime beats 74.67
+memory beats 59.90
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode()
+        result = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                result.next = l1
+                l1 = l1.next
+            else:
+                result.next = l2
+                l2 = l2.next
+            result = result.next
+
+        result.next = l1 or l2
+
+        return dummy.next
